536532f6e8e7a09bee9dea394695c0df49248671
hoang <hoang@DESKTOP-APH6IS0>
2018-12-02 17:25:50 +0700

    lolol

--- TEST/UserControl_QuanLyNhanVienBacSi.Designer.cs
+++ TEST/UserControl_QuanLyNhanVienBacSi.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(232, 164);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(214, 125);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // UserControl_QuanLyNhanVienBacSi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
             this.Name = "UserControl_QuanLyNhanVienBacSi";
-            this.Size = new System.Drawing.Size(298, 215);
+            this.Size = new System.Drawing.Size(603, 437);
             this.Load += new System.EventHandler(this.UserControl_QuanLyNhanVienBacSi_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
