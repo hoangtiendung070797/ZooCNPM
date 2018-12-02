fb72859d625e993416cc8bd327f1630ac7cda12d
hoang <hoang@DESKTOP-APH6IS0>
2018-12-02 17:24:40 +0700

    2222

---
+++ TEST/UserControl_QuanLyNhanVienBacSi.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace TEST
+{
+    partial class UserControl_QuanLyNhanVienBacSi
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UserControl_QuanLyNhanVienBacSi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UserControl_QuanLyNhanVienBacSi";
+            this.Size = new System.Drawing.Size(298, 215);
+            this.Load += new System.EventHandler(this.UserControl_QuanLyNhanVienBacSi_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
